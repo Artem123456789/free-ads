5d309ad999166d37c034f461fbf650dab5cdf393
artem <neznajkinartem357@gmail.com>
2019-12-10 17:43:51 +0600

    Comments class added

---
+++ src/app/classes/comment/comment.ts
@@ -0,0 +1,47 @@
+export class Comment {
+
+    private _id: number;
+    private _authorName: string;
+    private _text: string;
+    private _date: Date;
+
+    constructor(id: number, authorName: string, text: string, date: Date){
+        this.id = id;
+        this.authorName = authorName;
+        this.text = text;
+        this.date = date;
+    }
+
+    public get id(){
+        return this._id;
+    }
+
+    public get authorName(){
+        return this._authorName;
+    }
+
+    public get text(){
+        return this._text;
+    }
+
+    public get date(){
+        return this._date;
+    }
+
+    public set id(id: number){
+        this._id = id;
+    }
+
+    public set authorName(authorName: string){
+        this._authorName = authorName;
+    }
+
+    public set text(text: string){
+        this._text = text;
+    }
+
+    public set date(date: Date){
+        this._date = Object.assign({}, date);
+    }
+
+}
