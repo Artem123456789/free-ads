262b4b8b5da11b3ce0b898cd4ff7f700df69f82b
artem <neznajkinartem357@gmail.com>
2019-12-10 19:19:57 +0600

    Sign up component ready

--- src/app/classes/user/user.ts
+++ src/app/classes/user/user.ts
@@ -1,2 +1,47 @@
 export class User {
+
+    private _id: number;
+    private _name: string;
+    private _email: string;
+    private _password: string;
+
+    constructor(id: number = 0, name: string = "", email: string = "", password: string = ""){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public get id(){
+        return this._id;
+    }
+
+    public get name(){
+        return this._name;
+    }
+
+    public get email(){
+        return this._email;
+    }
+
+    public get password(){
+        return this._password;
+    }
+
+    public set id(id: number){
+        this._id = id;
+    }
+
+    public set name(name: string){
+        this._name = name;
+    }
+
+    public set email(email: string){
+        this._email = email;
+    }
+
+    public set password(password: string){
+        this._password = password;
+    }
+
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../classes/user/user';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.getUsers().subscribe(data=>{
+      this.users = Object.assign(this.users, data);
+    });
+    this.newUser = new User(this.users.length, "Васильев Василий Васильевич", "vasiliev@mail.com", "dwsedw");
+  }
 
   ngOnInit() {
+  }
+
+  private newUser: User;
+  private users: any = [];
+
+  private getUsers(){
+    const url = 'assets/users.json';
+    return this.http.get(url);
+  }
+
+  private signUpUser(){
+    this.users.push({
+      id: this.newUser.id, 
+      name: this.newUser.name,
+      email: this.newUser.email,
+      password: this.newUser.password
+    });
+    console.log(this.users);
   }
 
 }
