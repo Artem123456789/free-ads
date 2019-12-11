f297591beb543050c9a6aba18a037b6698778006
artem <neznajkinartem357@gmail.com>
2019-12-11 19:36:31 +0600

    Added comments and single ad component

--- src/app/single-ad/single-ad.component.ts
+++ src/app/single-ad/single-ad.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdsService } from '../services/ads.service';
+import { Comment } from '../classes/comment/comment';
+import { Ad } from '../classes/ad/ad';
 
 @Component({
   selector: 'app-single-ad',
@@ -11,11 +13,13 @@ export class SingleAdComponent implements OnInit {
 
   constructor(private adsSerice: AdsService, private activatedRouter: ActivatedRoute) {
     this.adId = activatedRouter.snapshot.params['id'];
+    this.ad = this.adsSerice.ads[this.adId];
   }
 
   ngOnInit() {
   }
 
   private adId: number;
+  private ad: Ad;
 
 }
