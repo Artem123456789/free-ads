41498e5cb60067a30683504d572516047d14afb8
artem <neznajkinartem357@gmail.com>
2019-12-11 18:19:30 +0600

    some html and css added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartPageComponent } from './start-page/start-page.component';
-
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+ 
 const routes: Routes = [
-  {path: 'start', component: StartPageComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'start', component: StartPageComponent }
 ];
 
 @NgModule({
