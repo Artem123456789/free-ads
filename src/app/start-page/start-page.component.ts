49283d24cd08c16875fb71254994ea4e16b0a545
artem <neznajkinartem357@gmail.com>
2019-12-11 16:57:39 +0600

    Added routing, and cactegory proprty to ads.json

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdsService } from '../services/ads.service';
 
 @Component({
   selector: 'app-start-page',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adsService: AdsService) { 
+  }
 
   ngOnInit() {
   }
